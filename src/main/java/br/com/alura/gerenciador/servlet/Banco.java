5c53674e1804d8c1c72e1c18ea5db1696d9ce287
Rinaldo Medeiros <rinaldo.rmedeiros@gmail.com>
2021-04-14 22:12:53 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,56 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		empresa.setId(chaveSequencial++);
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		empresa2.setId(chaveSequencial++);
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+
+	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
+		Banco.lista.add(empresa);
+	}
+	
+	
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+		
+	}
+
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+
+	public Empresa buscaEmpresaPorId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+
+}
